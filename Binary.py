44300ffd9607f45c19523799b2d3d1573b5e872f
CatastrophicPug <87585367+CatastrophicPug@users.noreply.github.com>
2021-08-09 20:58:39 +1000

    Add files via upload

---
+++ Binary.py
@@ -0,0 +1,18 @@
+# Important Code
+Yes = 1
+No = 0
+binaryVersionTwo = 'yes'
+
+# Commands
+
+print('After this, use binary numbers to make a word! (Max is 5 letters, include them in sets of 8 numbers).')
+while binaryVersionTwo == 'yes':
+    Binary = input(' ')
+    if Binary == '00000001':
+        print('1')
+    elif Binary == '00000000':
+        print('0')
+    elif Binary == '00000010':
+        print('2')
+    elif Binary == '00000011':
+        print('3')
